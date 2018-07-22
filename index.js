7b8c8a30399d523bbfc48206c560b3a4a1bfa6cc
Noelle Foster <uufarmgirl@gmail.com>
2018-07-22 16:38:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function placeOrder(cardNumber) {
     return `Your total cost is ${total()}, which will \
     be charged to the card ${cardNumber}.`
   }
-    
+
   }
   // write your code here
 }
