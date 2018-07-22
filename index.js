3468085c9fb953ff0e10df1fff70f5c1b80b81f9
Noelle Foster <uufarmgirl@gmail.com>
2018-07-22 16:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,12 +74,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber.IsNullOrEmpty){
+  if(cardNumber.IsNullOrEmpty()){
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
-    var str= `Your total cost is ${total()}, which will \
-    be charged to the card ${cardNumber}.`
+    var str= `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
     cart= new Array()
     return str
   }
