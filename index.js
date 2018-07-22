e268560c2f432ee709afa36253affed155060443
Noelle Foster <uufarmgirl@gmail.com>
2018-07-22 16:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,9 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
-    return `Your total cost is ${total()}, which will \
+    str= `Your total cost is ${total()}, which will \
     be charged to the card ${cardNumber}.`
+    cart= new Array)_
+    return str
   }
 }
